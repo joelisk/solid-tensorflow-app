2bab38cc0dcc07cd26bed8820f41e0081455e44b
Joe Lisk <josephlisk13@gmail.com>
2022-12-15 15:07:33 -0600

    adding tensorflow.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import type { Component } from 'solid-js';
-
+import { Component, createEffect } from 'solid-js';
 import logo from './logo.svg';
 import styles from './App.module.css';
+
+import model from './model';
+console.log(model)
+
+//TODO: find way to display model
+
+// 'await' has no effect
+// createEffect(async () => {
+//   await model;
+//   Promise.all()
+// });
 
 const App: Component = () => {
   return (
@@ -24,4 +34,15 @@ const App: Component = () => {
   );
 };
 
+// type 'Sequential' is not assignable to type 'Element'
+// const ModelTable: Component = () => {
+//   return (
+//     <div>
+//       {model}
+//     </div>
+//   );
+// };
+
 export default App;
+
+// npm run dev
